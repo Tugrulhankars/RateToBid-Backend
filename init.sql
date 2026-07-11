6ab9272aa780fce089e23c213f2037ab95d53fc4
Tugrulhan Karsli <karslitugrulhan@gmail.com>
2026-07-11 22:58:44 +0300

    init.sql added

---
+++ init.sql
@@ -0,0 +1,10 @@
+INSERT INTO categories (name, created_at, updated_at)
+VALUES
+    ('Elektronik', NOW(), NOW()),
+    ('Moda', NOW(), NOW()),
+    ('Otomobil', NOW(), NOW()),
+    ('Ev & Yaşam', NOW(), NOW()),
+    ('Koleksiyon', NOW(), NOW()),
+    ('Spor', NOW(), NOW()),
+    ('Oyun', NOW(), NOW()),
+    ('Kitap', NOW(), NOW());
